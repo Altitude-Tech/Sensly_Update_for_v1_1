4fd1d92142bdd403e75a9afe4195945468073f77
samAlti <sam@altitude.pw>
2018-08-29 15:15:53 +0100

    Update
    Modified so that the RPi's bluetooth interface isn't disabled

--- Uart_Enable_Sensly_RPi3.sh
+++ Uart_Enable_Sensly_RPi3.sh
@@ -6,11 +6,11 @@ case $yn in
 	[Yy]* )echo "Have you modified the /boot/cmdline.txt or /boot/config.txt file?"; read -p " " yn;
 		case $yn in
 			[Yy]* )sudo cp /boot/config.txt /boot/config.old && sudo cp /boot/cmdline.txt /boot/cmdline.old; break;;
-			[Nn]* )break;;
+			[Nn]* );;
 		esac
 		echo " " >> /boot/config.txt;
 		echo "# Uart Enable" >> /boot/config.txt;
-		echo "dtoverlay=pi3-disable-bt" >> /boot/config.txt
+		echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
 		echo "enable_uart=1" >> /boot/config.txt;
 		sed -i "s/console=tty1//g" /boot/cmdline.txt
 		sed -i "s/console=serial0/console=tty1/g" /boot/cmdline.txt
